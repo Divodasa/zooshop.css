5c58bfdef27a45ca2b53a46dd7fc2a142234ab43
divodasa <brahmajoytee@gmail.com>
2018-09-22 22:00:02 +0300

    HW*

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-let menu1 = '<a href="../index.html"> Главная </a>| <a href="../html/mkorder.html"> Страница заказа </a>| <a href="../html/phgallery.html"> Фотогалерея </a>| <a href="../html/peace.html"> Кот в ананде </a>| <a href="../html/feedback1.html"> Обратная связь </a>| <a href="../html/contactus.html"> Контакты </a>';
+const menu1 = '<a href="../index.html"> Главная </a>| <a href="../html/mkorder.html"> Страница заказа </a>| <a href="../html/phgallery.html"> Фотогалерея </a>| <a href="../html/peace.html"> Кот в ананде </a>| <a href="../html/feedback1.html"> Обратная связь </a>| <a href="../html/contactus.html"> Контакты </a>';
 
 function callMenuHref() {
     return menu1;
